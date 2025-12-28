6c21bc075c189a55b001cf273b68ecb8903c79fd
Laksh Anil Chinchmalatpure <lakshchinchmalatpure@gmail.com>
2025-12-28 19:17:00 +0530

    Implement SumFirstN to calculate sum of first N numbers

--- loops/SumFirstN.java
+++ loops/SumFirstN.java
@@ -1,1 +1,13 @@
+public class SumFirstN {
+    public static void main(String[] args) {
+        int n = 10;
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum of first N numbers: " + sum);
+    }
+}
 
